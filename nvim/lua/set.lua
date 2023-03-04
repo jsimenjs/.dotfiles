ea6a066d6f5d62671357d7d4e279270613c2d7fc
Simen Severinsen <32468772+esaji@users.noreply.github.com>
2023-03-04 01:14:05 +0100

    Added undotree, lsp, harpoon and git integration

--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -9,10 +9,12 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-
+vim.opt.scrolloff = 8
+vim.opt.termguicolors = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
-
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
 --vim.o.background = "dark" -- or "light" for light mode
 vim.cmd("colorscheme kanagawa")
 vim.g.mapleader = " "
