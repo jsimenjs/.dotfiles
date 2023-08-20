f16cefdea4ab99195197fe42a012e7b34b400c7b
Simen Severinsen <32468772+esaji@users.noreply.github.com>
2023-08-20 23:20:48 +0200

    feat: add keymap descriptions and additional keymapping

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {desc = 'find files'})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {desc = 'fuzzy search git ls-files output'})
+vim.keymap.set('n', '<leader>ps',  function()
     builtin.grep_string({ search = vim.fn.input("Grep > ")});
-end)
+end, {desc = 'search string from cursor/selection in cwd'})
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, {desc = 'search string in cwd'})
